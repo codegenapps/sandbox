7a82ba90f55168bbfd4860ddaa465931fcb0e39e
Andy <wang.shih.hua.tw@gmail.com>
2026-05-11 20:01:50 +0800

    修改api 串接的沙盒初始化

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
+import Script from 'next/script';
 import { NextUIProvider } from '@nextui-org/react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { useRouter } from 'next/router';
@@ -12,8 +13,9 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <QueryClientProvider client={queryClient}>
       <Head>
-        <script src="/cga-inspector.js" defer></script>
+        <title>CGA Shadow Node</title>
       </Head>
+      <Script src="/cga-inspector.js" strategy="afterInteractive" />
       <NextUIProvider navigate={router.push}>
         <main className='dark text-foreground bg-background min-h-screen' suppressHydrationWarning>
           <Component {...pageProps} />
